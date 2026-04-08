77f0b03749d637ee251976d0ac8ba687e5f64b9e
Anees shaikh <aneesshaikh329@gmail.com>
2026-04-08 18:30:02 +0530

    changed public url

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2')
 require('dotenv').config()
 
 const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
+  host: process.env.MYSQL_PUBLIC_URL,
   port: process.env.MYSQLPORT,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
