c05c7eb35d292343a3d3e84ee24f4fa5768650af
Anees shaikh <aneesshaikh329@gmail.com>
2026-04-08 18:33:48 +0530

    fix: Update database host configuration to use correct environment variable

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2')
 require('dotenv').config()
 
 const db = mysql.createConnection({
-  host: process.env.MYSQL_PUBLIC_URL,
+  host: process.env.MYSQLHOST,
   port: process.env.MYSQLPORT,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
